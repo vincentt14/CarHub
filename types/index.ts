6b9a1646c76524c97398ce5d915e7702074aa519
Vincent <vincent14@student.umn.ac.id>
2023-06-28 15:07:57 +0700

    implement combobox in searchmanufacturer

--- types/index.ts
+++ types/index.ts
@@ -9,5 +9,5 @@ export interface CustomButtonProps {
 
 export interface SearchManufacturerProps {
   manufacturer: String;
-  setManufacturer: (manufacturer: String) => void;
+  setManuFacturer: (manufacturer: String) => void;
 }
