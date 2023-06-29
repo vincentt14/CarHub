4c9c1cdcc3d1eee5e37d5a04101d3acaa5c241e1
Vincent <vincent14@student.umn.ac.id>
2023-06-29 22:04:57 +0700

    server side rendering fetch data by params

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,22 @@ import CarCard from "@/components/CarCard";
 import CustomFilter from "@/components/CustomFilter";
 import Hero from "@/components/Hero";
 import SearchBar from "@/components/SearchBar";
+import { HomeProps } from "@/types";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+  // console.log(allCars);
 
   return (
     <main className="overflow-hidden">
